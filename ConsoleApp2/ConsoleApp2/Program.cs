a0c3d7be3319ea2fcbe43852d66a4db42c0b8880
Harun-Gundogmus <gundogmusharun2@gmail.com>
2022-12-19 14:05:44 +0300

    first-commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen Yaşınınızı Giriniz: ");
+            int yas = Convert.ToInt32(Console.ReadLine());
+            int sonuc = 20 - yas;
+            if (sonuc == 0)
+            {
+                Console.WriteLine("Askerlik Zamanı");
+            } 
+            else if (sonuc > 20)
+            {
+                Console.WriteLine("Askerlik Şubesine başvurun");
+            }
+            else if (sonuc < 20)
+            {
+                Console.WriteLine("Askere Gitmenize {0} yıl var.", sonuc);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
